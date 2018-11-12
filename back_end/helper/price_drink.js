200c4b22e91dda53bed3bedac13f3f03a9079c51
Alexandre Falardeau <alexandre.h.falardeau@gmail.com>
2018-11-12 12:52:10 -0500

    Added a rum recipe seed that is nearly complete

---
+++ back_end/helper/price_drink.js
@@ -0,0 +1,47 @@
+/**
+ * 
+ * @todo: Add a portions and price property on the recipe object
+ * 
+ * @todo: GET request for a certain recipe (should send a json containing all recipes)
+ * @todo: GET request for a menu (containing only recipe ids and menu information)
+ * @todo: POST request for a new recipe
+ * @todo: 
+ */
+
+function convert(from, to, cost) {
+    // Add conversions when necessary
+}
+
+/**
+ * @throws A nonConversionError if conversion does not exist (drink will be priced at 0)
+ * 
+ * @todo: needs to be tested
+ * @param {RecipeDoc} recipe 
+ */
+function pricePortion(recipe) {
+    
+    let price = 0;
+    recipe.ingredients.forEach((ingredient)=>{
+        let ingredientCost = ingredient.ingredient.price.cost * ingredient.quantity;
+        if (ingredient.unitOfMeasue != ingredient.ingredient.price.unitOfMeasue)
+            ingredientCost = convert(ingredient.ingredient.price.unitOfMeasue,
+                ingredient.unitOfMeasue, 
+                ingredient.ingredient.price.cost ) *ingredient.quantity;
+        price += ingredientCost;
+    })
+    return price/recipe.portions;
+}
+
+module.exports = pricePortion;
+
+// Need to use jest for this
+// function testPriceDrink() {
+//     let test_recipe = require("../seeds/recipe_rum_smash.json");
+//     if (pricePortion(test_recipe) == test_recipe.price) {
+//         console.log("test passed");
+//     } else {
+//         console.log("test failed");
+//     }
+// }
+
+// testPriceDrink();
--- back_end/routes/recipes.js
+++ back_end/routes/recipes.js
@@ -1,17 +1,38 @@
 let express = require("express");
 let router = express.Router();
 
+let pricePortion = require("../helper/price_drink.js");
+
 function newId() {
     return Math.floor(Math.random()*100000).toString();
 }
+
+
 
 router.get("/:recipe_id", function(req,res){
     fetchRecipe(req.params.recipe_id)
     .then((recipeDoc)=>{
         res.json(recipeDoc);
     })
+    // .catch((err)=>{
+    //     res.json(err);
+    // })
 })
 
+router.get("/menu/:menu_id", function(req,res){
+    fetchMenu(req.params.menu_id)
+    .then((menuDoc)=>{
+        res.json(menuDoc);
+    })
+    // .catch((err)=>{
+    //     res.json(err);
+    // })
+})
+
+router.post("/", function(req,res){
+    recipe=req.body;
+    // ...
+})
 
 
 function fetchRecipe(recipeId) {
